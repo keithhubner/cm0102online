e8691d1f9f26cf92c03470c567a48ba88dfa4e0f
keithhubner <32544724+keithhubner@users.noreply.github.com>
2024-01-22 22:46:20 +0000

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,6 @@ sleep 1s
 mkdir /cm0102/.vnc
 
 Xvfb :99 &
-DISPLAY=:99 wine $WINEPREFIX/dosdevices/d:/cm0102.exe -nosound &
+DISPLAY=:99 wine $WINEPREFIX/dosdevices/d:/cm0102_GDI.exe -nosound &
 x11vnc -storepasswd $VNCPASSWORD /cm0102/.vnc/passwd &
 x0vncserver -passwordfile /cm0102/.vnc/passwd -display :99
